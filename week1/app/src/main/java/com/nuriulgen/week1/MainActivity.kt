83653ba521ceaa96568ef330c909b590effe0384
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-08-22 17:44:51 +0300

    some changes added

--- week1/app/src/main/java/com/nuriulgen/week1/MainActivity.kt
+++ week1/app/src/main/java/com/nuriulgen/week1/MainActivity.kt
@@ -7,21 +7,27 @@ import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    //Variables
     private lateinit var button2 : Button
     private lateinit var image : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //id of used variables
         button2 = findViewById(R.id.button2)
         image = findViewById(R.id.imageView)
 
+        //actions to be taken on button click event
         button2.setOnClickListener{
             changeImage()
         }
     }
 
+    //image change method
     private fun changeImage() {
+        // Generating random numbers between 1 and 6 and assigning them to the variable
         val drawImage = when(Random().nextInt(6) +1){
             1 -> R.drawable.ic_dice_1
             2 -> R.drawable.ic_dice_2
@@ -31,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 
             else -> R.drawable.ic_dice_6
         }
+    
         image.setImageResource(drawImage)
     }
 }
