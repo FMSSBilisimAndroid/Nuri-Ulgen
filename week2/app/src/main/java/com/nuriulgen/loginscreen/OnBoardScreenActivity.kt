3d5358819069c4c2137a39c5a7d76491e63bec61
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-08-29 00:54:53 +0300

    OnBoard screen completed.

--- week2/app/src/main/java/com/nuriulgen/loginscreen/OnBoardScreenActivity.kt
+++ week2/app/src/main/java/com/nuriulgen/loginscreen/OnBoardScreenActivity.kt
@@ -3,9 +3,9 @@ package com.nuriulgen.loginscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnBoardScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_on_board_screen)
     }
 }
---
+++ week2/app/src/main/java/com/nuriulgen/loginscreen/SignUpScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.nuriulgen.loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_screen)
+    }
+}
